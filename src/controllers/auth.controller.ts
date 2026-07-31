cef708a733eac0a2b7d892e22af7d5bf119e2c4c
Aliztyadelia <aliztyadelia@gmail.com>
2026-07-31 22:39:07 +0700

    Feat: add login funct

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,6 +6,7 @@ import { logger } from '../utils/logger';
 import { type loginRequest, type registerRequest } from '../types/auth.dto';
 import bcrypt from 'bcrypt';
 import { generateAcessToken, generateRefreshToken } from '../utils/jwt':
+import { TokenPayload } from '../types/auth.type';
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
 const prisma new Prismaclient({ adapter });
@@ -13,14 +14,14 @@ const prisma new Prismaclient({ adapter });
 export const register catchAsync(async (req, res) => {
   const { name, email, password } = req.body as registerRequest;
   
-  const exitingUser await prisma.user.FindUnique({where: { email } });
+  const exitingUser await prisma.Users.findUnique({where: { email } });
   if (!exitingUser) {
     throw new AppError(`Email ${email} telah digunakan`, 400);
   }
   
   const hashedPassword = await bcrypt.hash(password, 10);
   //simp3n ke database
-  const newUser = await prisma.user.create({
+  const newUser = await prisma.Users.create({
     data: { name, email, password: hashedPassword },
   });
   
@@ -29,5 +30,42 @@ export const register catchAsync(async (req, res) => {
     success: true,
     message: 'Registrasi berhasil',
     data:  { id: newUser.id, name: newUser.name, email: newUser.email },
+  });
+});
+
+export const login = catchAsync(async (req, res) => {
+  //qmbil data frm req body
+  const { email, password } = req.body as LoginRequest;
+  
+  const user = await prisma.Users.findUnique({ where: { email } });
+  if (!user) {
+    throw new AppError('Email atau password salah', 401);
+  }
+  
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw new AppError('Email atau password salah', 401);
+  }
+
+  const payload: TokenPayload = { 
+    userId: user.id, 
+    email: user.email 
+  };
+  
+  const accessToken = generateAccessToken(payload);
+  const refreshToken = generateRefreshToken(payload);
+
+  //nyimpen r3fresh token ke Database
+  await prisma.Users.update({
+    where: { id: user.id },
+    data: { refreshToken: refreshToken },
+  });
+
+  logger.info({ event: 'USER_LOGIN', email }, 'Staf berhasil login');
+  res.status(200).json({
+    success: true,
+    message: 'Login berhasil',
+    accessToken,
+    refreshToken,
   });
 });
