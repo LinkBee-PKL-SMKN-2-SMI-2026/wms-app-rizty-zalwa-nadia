36449c06e77ce6ef54777f442601b4557361bd27
Aliztyadelia <aliztyadelia@gmail.com>
2026-08-02 00:25:37 +0700

    chore: remove unused comments

--- src/middlewares/authorize.middleware.ts
+++ src/middlewares/authorize.middleware.ts
@@ -12,17 +12,15 @@ export const authorize = (...roles: string[]) => {
     if (!req.user) {
       return next(new AppError('Unauthorized', 401));
     }
-    // Ambil role user dari database berdasarkan req.user.userId
+    // Ambil role user dari database
     const user = await prisma.users.findUnique({
       where: { id: req.user.userId },
     });
     
-    // Jika role tidak ada di dalam array roles, lempar AppError 403
     if (!user) {
       return next(new AppError('User tidak ditemukan', 404));
     }
     
-    //Cek apakah role user ada di dalam daftar roles yang diizinkan
     if (!roles.includes(user.role)) {
       return next(new AppError('Forbidden: Anda tidak memiliki akses ke fitur ini', 403));
     }
