abccf9e0112fd76ba18de6792c424fb1a363dd74
Aliztyadelia <aliztyadelia@gmail.com>
2026-08-01 12:11:36 +0700

    fix: update authorize middleware logic

--- src/middlewares/authorize.middleware.ts
+++ src/middlewares/authorize.middleware.ts
@@ -4,6 +4,21 @@ export const authorize = (...roles: string[]) => {
       return next(new AppError('Unauthorized', 401));
     }
     // Ambil role user dari database berdasarkan req.user.userId
+    const user = await prisma.users.findUnique({
+      where: { id: req.user.userId },
+    });
+    
     // Jika role tidak ada di dalam array roles, lempar AppError 403
+    if (!user) {
+      return next(new AppError('User tidak ditemukan', 404));
+    }
+    
+    //Cek apakah role user ada di dalam daftar roles yang diizinkan
+    if (!roles.includes(user.role)) {
+      return next(new AppError('Forbidden: Anda tidak memiliki akses ke fitur ini', 403));
+    }
+    
+    controller
+    next();
   };
 };
