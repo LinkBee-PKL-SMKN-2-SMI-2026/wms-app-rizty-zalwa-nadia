0fe6ea4182cbe1f57c49eb60ef20c20024ef94be
iPixell <aliztyadelia@gmail.com>
2026-08-11 14:59:50 +0700

    feat: get by id

--- src/validations/category.validation.ts
+++ src/validations/category.validation.ts
@@ -6,3 +6,19 @@ export const CreateCategorySchema = z.object({
     description: z.string().optional(),
   }),
 });
+
+export const GetAllCategorySchema = z.object({
+  query: z.object({
+    page: z.coerce.number().int().min(1).default(1),
+    limit: z.coerce.number().int().min(1).max(100).default(10),
+    search: z.string().optional(),
+    sort: z.string().optional(),
+  }),
+});
+
+export const GetCategoryByIdSchema = z.object({
+ params: z.object({
+  id: z.string().uuid(`ID kategori tidak valid`),
+ }),
+});
+
