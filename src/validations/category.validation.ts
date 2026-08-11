643abb078334a4d0948014c25033f78a550ea0c6
iPixell <aliztyadelia@gmail.com>
2026-08-11 16:38:38 +0700

    feat: updt min char frm 2 to 3

--- src/validations/category.validation.ts
+++ src/validations/category.validation.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const CreateCategorySchema = z.object({
   body: z.object({
-    name: z.string().min(2, 'Nama minimal 2 karakter'),
+    name: z.string().min(3, 'Nama minimal 3 karakter'),
     description: z.string().optional(),
   }),
 });
@@ -27,7 +27,7 @@ export const UpdateCategorySchema = z.object({
     id: z.string().uuid(`ID kategori tidak valid`),
   }),
   body: z.object({
-    name: z.string().min(2, 'Nama minimal 2 karakter'),
+    name: z.string().min(3, 'Nama minimal 3 karakter'),
     description: z.string().optional(),
     isActive: z.boolean().optional(),
   }),
