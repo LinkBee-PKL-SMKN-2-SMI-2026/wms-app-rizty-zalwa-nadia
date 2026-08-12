0c15eb92203b7b0e1a43e556ea252d403217ff21
iPixell <aliztyadelia@gmail.com>
2026-08-13 06:59:40 +0700

    fix: PROMISE IT'S THE LAST TYPO??

--- src/models/location.dto.ts
+++ src/models/location.dto.ts
@@ -10,6 +10,6 @@ import {
 export type CreateLocationRequest = z.infer<typeof CreateLocationSchema>['body'];
 export type GetAllLocationRequest = z.infer<typeof GetAllLocationSchema>['query'];
 export type GetLocationByIdRequest = z.infer<typeof GetLocationByIdSchema>['params'];
-export type UpdateLocationRequest = z.infer<typeof UpdateaLocationSchema>['body'];
+export type UpdateLocationRequest = z.infer<typeof UpdateLocationSchema>['body'];
 export type UpdateLocationParams = z.infer<typeof UpdateLocationSchema>['params'];
 export type DeleteLocationRequest = z.infer<typeof DeleteLocationSchema>['params'];
