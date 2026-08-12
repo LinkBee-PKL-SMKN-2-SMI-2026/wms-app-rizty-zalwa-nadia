59e18ae68b91cbe6d837d6ad387e03d9e4c85de1
iPixell <aliztyadelia@gmail.com>
2026-08-13 06:55:07 +0700

    fix: blum diubah ke orodut T_T

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,40 +1,63 @@
 import { Router } from 'express';
+
 import { validate } from '../middlewares/validate.middleware';
 import { authenticate } from '../middlewares/authenticate.middleware';
 import { authorize } from '../middlewares/authorize.middleware';
+
 import {
-  CreateLocationSchema,
-  GetAllLocationSchema,
-  GetLocationByIdSchema,
-  UpdateLocationSchema,
-  DeleteLocationSchema,
-} from '../validations/location.validation';
+  CreateProductSchema,
+  GetAllProductSchema,
+  GetProductByIdSchema,
+  UpdateProductSchema,
+  DeleteProductSchema,
+} from '../validations/product.validation';
+
 import {
-  createLocation,
-  getAllLocations,
-  getLocationById,
-  updateLocation,
-  deleteLocation,
-} from '../controllers/location.controller';
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} from '../controllers/product.controller';
 
 const router = Router();
 
-router.post('/', authenticate, authorize('ADMIN'), validate(CreateLocationSchema), createLocation);
-router.get('/', authenticate, validate(GetAllLocationSchema), getAllLocations);
-router.get('/:id', authenticate, validate(GetLocationByIdSchema), getLocationById);
+router.post(
+  '/',
+  authenticate,
+  authorize('ADMIN'),
+  validate(CreateProductSchema),
+  createProduct,
+);
+
+router.get(
+  '/',
+  authenticate,
+  validate(GetAllProductSchema),
+  getAllProducts,
+);
+
+router.get(
+  '/:id',
+  authenticate,
+  validate(GetProductByIdSchema),
+  getProductById,
+);
+
 router.put(
   '/:id',
   authenticate,
   authorize('ADMIN'),
-  validate(UpdateLocationSchema),
-  updateLocation,
+  validate(UpdateProductSchema),
+  updateProduct,
 );
+
 router.delete(
   '/:id',
   authenticate,
   authorize('ADMIN'),
-  validate(DeleteLocationSchema),
-  deleteLocation,
+  validate(DeleteProductSchema),
+  deleteProduct,
 );
 
 router.all('/*path', (_req, res) => {
